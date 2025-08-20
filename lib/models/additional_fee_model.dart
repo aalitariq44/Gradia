1630a34b4572e9781e10f2614501632abeb8af0c
ali tariq pc <aalmali9000@gmail.com>
2025-08-20 09:46:10 +0300

    تعدبلات

--- lib/models/additional_fee_model.dart
+++ lib/models/additional_fee_model.dart
@@ -45,7 +45,7 @@ class AdditionalFee {
       id: map['id'],
       studentId: map['student_id'],
       feeType: map['fee_type'],
-      amount: map['amount'],
+      amount: (map['amount'] as num).toDouble(),
       paid: map['paid'] == 1,
       paymentDate: map['payment_date'] != null
           ? DateTime.parse(map['payment_date'])
